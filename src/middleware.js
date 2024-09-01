cc9cf4c99c42b0a21eba439de7882a387577ba4a
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2024-09-01 16:47:42 +0700

    v535

--- src/middleware.js
+++ src/middleware.js
@@ -2,20 +2,21 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 export { default } from "next-auth/middleware";
 
-export async function middleware(req) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+// export async function middleware(req) {
+//     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-    if (
-        token &&
-        (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/register")
-    ) {
-        return NextResponse.redirect(new URL("/", req.url)); // Redirect to homepage or another page
-    }
+//     if (
+//         token &&
+//         (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/register")
+//     ) {
+//         return NextResponse.redirect(new URL("/", req.url)); // Redirect to homepage or another page
+//     }
 
-    // Default behavior for other pages (authenticated users should be able to access)
-    return NextResponse.next();
-}
+//     // Default behavior for other pages (authenticated users should be able to access)
+//     return NextResponse.next();
+// }
 
-export const config = {
-    matcher: ["/login", "/register", "/((?!register|login).*)"], // Ensure the middleware applies to all pages
-};
+// export const config = {
+//     matcher: ["/login", "/register", "/((?!register|login).*)"], // Ensure the middleware applies to all pages
+// };
+export const config = { matcher: ["/((?!register|login).*)"] };
